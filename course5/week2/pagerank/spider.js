d5d6538c8ca8dd4fd4443f7b02ef72fbb360b2e2
Vlad Flore <flore.vlad@gmail.com>
2019-12-29 20:37:29 +0100

    add python code from course 5 week 2

---
+++ course5/week2/pagerank/spider.js
@@ -0,0 +1,9 @@
+spiderJson = {"nodes":[
+{"weight":1,"rank":0.0, "id":7, "url":"http://python-data.dr-chuck.net/known_by_42.html"},
+{"weight":1,"rank":0.0, "id":14, "url":"http://python-data.dr-chuck.net/known_by_Ryden.html"},
+{"weight":1,"rank":0.0, "id":65, "url":"http://python-data.dr-chuck.net/known_by_Jo.html"},
+{"weight":1,"rank":19.0, "id":143, "url":"http://python-data.dr-chuck.net/known_by_Raashi.html"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":2,"target":3,"value":3}]};
