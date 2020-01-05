2992b876102291df70d9cb681978a8369cd9de5b
Vlad Flore <flore.vlad@gmail.com>
2020-01-05 19:07:19 +0100

    add python code from course 5 week 4

--- course5/week4/gmane/gmane.py
+++ course5/week4/gmane/gmane.py
@@ -1,11 +1,10 @@
-import sqlite3
-import time
-import ssl
-import urllib.request, urllib.parse, urllib.error
-from urllib.parse import urljoin
-from urllib.parse import urlparse
 import re
-from datetime import datetime, timedelta
+import sqlite3
+import ssl
+import time
+import urllib.error
+import urllib.request
+from datetime import datetime
 
 # Not all systems have this so conditionally define parser
 try:
@@ -13,7 +12,8 @@ try:
 except:
     pass
 
-def parsemaildate(md) :
+
+def parsemaildate(md):
     # See if we have dateutil
     try:
         pdate = parser.parse(tdate)
@@ -29,16 +29,16 @@ def parsemaildate(md) :
 
     # Try a bunch of format variations - strptime() is *lame*
     dnotz = None
-    for form in [ '%d %b %Y %H:%M:%S', '%d %b %Y %H:%M:%S',
-        '%d %b %Y %H:%M', '%d %b %Y %H:%M', '%d %b %y %H:%M:%S',
-        '%d %b %y %H:%M:%S', '%d %b %y %H:%M', '%d %b %y %H:%M' ] :
+    for form in ['%d %b %Y %H:%M:%S', '%d %b %Y %H:%M:%S',
+                 '%d %b %Y %H:%M', '%d %b %Y %H:%M', '%d %b %y %H:%M:%S',
+                 '%d %b %y %H:%M:%S', '%d %b %y %H:%M', '%d %b %y %H:%M']:
         try:
             dnotz = datetime.strptime(notz, form)
             break
         except:
             continue
 
-    if dnotz is None :
+    if dnotz is None:
         # print 'Bad Date:',md
         return None
 
@@ -47,15 +47,16 @@ def parsemaildate(md) :
     tz = "+0000"
     try:
         tz = pieces[4]
-        ival = int(tz) # Only want numeric timezone values
-        if tz == '-0000' : tz = '+0000'
+        ival = int(tz)  # Only want numeric timezone values
+        if tz == '-0000': tz = '+0000'
         tzh = tz[:3]
         tzm = tz[3:]
-        tz = tzh+":"+tzm
+        tz = tzh + ":" + tzm
     except:
         pass
 
-    return iso+tz
+    return iso + tz
+
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -73,33 +74,33 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Messages
 
 # Pick up where we left off
 start = None
-cur.execute('SELECT max(id) FROM Messages' )
+cur.execute('SELECT max(id) FROM Messages')
 try:
     row = cur.fetchone()
-    if row is None :
+    if row is None:
         start = 0
     else:
         start = row[0]
 except:
     start = 0
 
-if start is None : start = 0
+if start is None: start = 0
 
 many = 0
 count = 0
 fail = 0
 while True:
-    if ( many < 1 ) :
+    if (many < 1):
         conn.commit()
         sval = input('How many messages:')
-        if ( len(sval) < 1 ) : break
+        if (len(sval) < 1): break
         many = int(sval)
 
     start = start + 1
-    cur.execute('SELECT id FROM Messages WHERE id=?', (start,) )
+    cur.execute('SELECT id FROM Messages WHERE id=?', (start,))
     try:
         row = cur.fetchone()
-        if row is not None : continue
+        if row is not None: continue
     except:
         row = None
 
@@ -111,79 +112,79 @@ while True:
         # Open with a timeout of 30 seconds
         document = urllib.request.urlopen(url, None, 30, context=ctx)
         text = document.read().decode()
-        if document.getcode() != 200 :
-            print("Error code=",document.getcode(), url)
+        if document.getcode() != 200:
+            print("Error code=", document.getcode(), url)
             break
     except KeyboardInterrupt:
         print('')
         print('Program interrupted by user...')
         break
     except Exception as e:
-        print("Unable to retrieve or parse page",url)
-        print("Error",e)
+        print("Unable to retrieve or parse page", url)
+        print("Error", e)
         fail = fail + 1
-        if fail > 5 : break
+        if fail > 5: break
         continue
 
-    print(url,len(text))
+    print(url, len(text))
     count = count + 1
 
     if not text.startswith("From "):
         print(text)
         print("Did not find From ")
         fail = fail + 1
-        if fail > 5 : break
+        if fail > 5: break
         continue
 
     pos = text.find("\n\n")
-    if pos > 0 :
+    if pos > 0:
         hdr = text[:pos]
-        body = text[pos+2:]
+        body = text[pos + 2:]
     else:
         print(text)
         print("Could not find break between headers and body")
         fail = fail + 1
-        if fail > 5 : break
+        if fail > 5: break
         continue
 
     email = None
     x = re.findall('\nFrom: .* <(\S+@\S+)>\n', hdr)
-    if len(x) == 1 :
+    if len(x) == 1:
         email = x[0];
         email = email.strip().lower()
-        email = email.replace("<","")
+        email = email.replace("<", "")
     else:
         x = re.findall('\nFrom: (\S+@\S+)\n', hdr)
-        if len(x) == 1 :
+        if len(x) == 1:
             email = x[0];
             email = email.strip().lower()
-            email = email.replace("<","")
+            email = email.replace("<", "")
 
     date = None
     y = re.findall('\Date: .*, (.*)\n', hdr)
-    if len(y) == 1 :
+    if len(y) == 1:
         tdate = y[0]
         tdate = tdate[:26]
         try:
             sent_at = parsemaildate(tdate)
         except:
             print(text)
-            print("Parse fail",tdate)
+            print("Parse fail", tdate)
             fail = fail + 1
-            if fail > 5 : break
+            if fail > 5: break
             continue
 
     subject = None
     z = re.findall('\Subject: (.*)\n', hdr)
-    if len(z) == 1 : subject = z[0].strip().lower();
+    if len(z) == 1: subject = z[0].strip().lower();
 
     # Reset the fail counter
     fail = 0
-    print("   ",email,sent_at,subject)
+    print("   ", email, sent_at, subject)
     cur.execute('''INSERT OR IGNORE INTO Messages (id, email, sent_at, subject, headers, body)
-        VALUES ( ?, ?, ?, ?, ?, ? )''', ( start, email, sent_at, subject, hdr, body))
-    if count % 50 == 0 : conn.commit()
-    if count % 100 == 0 : time.sleep(1)
+        VALUES ( ?, ?, ?, ?, ?, ? )''', (start, email, sent_at, subject, hdr, body))
+    if count % 50 == 0: conn.commit()
+    if count % 100 == 0: time.sleep(1)
 
 conn.commit()
 cur.close()
